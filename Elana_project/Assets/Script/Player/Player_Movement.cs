df4d67b366c71735e330144835df4a76eed7f878
MOuhchinat213 <djoumad.jugurta@gmail.com>
2025-11-23 01:58:05 +0100

    jumped fixed

--- Elana_project/Assets/Script/Player/Player_Movement.cs
+++ Elana_project/Assets/Script/Player/Player_Movement.cs
@@ -1,43 +1,73 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 
 
 public class Player_Movement : MonoBehaviour
 {
+    //COMPONENT
     public Rigidbody2D rb;
     public SpriteRenderer sr;
-    public Transform Camera;
+   
+   //MOVEMENT
     public int last_direction;
-    public float jump_speed=6;
     public float RunSpeed=6f;
+       
+    //public Animator animator;
+    //DASH
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.2f;
+    public bool isDashing = false;
+
+    //JUMPING
+    public bool isGrounded; 
+    public LayerMask LayerGround;
+    public Transform GroundCheck;
+    public int maxJumps = 1;
+    public int jumpsRemaining;
+    public float jump_speed=6;
+
 
     void Start()
     {
+        //animator= GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sr= GetComponent<SpriteRenderer>();
     }
-
-
     void Update()
     {
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, LayerGround);
+        if (isGrounded)
         {
-            Jump();
+            jumpsRemaining = maxJumps;
         }
-        Horizontal_Move();
+        if(Input.GetKeyDown(KeyCode.Space) && jumpsRemaining > 0)
+        {
+            
+            Jump();
+            
+        }
+
+            Horizontal_Move();
     }
     void Jump()
     {
-        float input= Input.GetAxisRaw("Horizontal");
-        rb.linearVelocityY = jump_speed;
-        rb.linearVelocityX=RunSpeed*input;     
-        
+            if(!isGrounded)
+                jumpsRemaining--;
+
+            float input= Input.GetAxisRaw("Horizontal");
+            //animator.SetTrigger("Jump");
+            rb.linearVelocityY = jump_speed;
+            rb.linearVelocityX=RunSpeed*input;  
+            isGrounded=false;  
     }
-    
+
+
     void Horizontal_Move()
     {
         float input = Input.GetAxis("Horizontal");
+        //animator.SetFloat("speed",Mathf.Abs(input));
         rb.linearVelocityX = RunSpeed*input;
                 if (input < 0)
         {
@@ -52,4 +82,7 @@ public class Player_Movement : MonoBehaviour
         sr.flipX = (last_direction == -1);
         
     }
+
+
+
 }
