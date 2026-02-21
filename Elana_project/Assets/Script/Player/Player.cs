a8d7c75f19e2513d9dff3eb6215b8d8f5ee772e0
jugurta-mohamed.djoumad <djoumad.jugurta@gmail.com>
2026-02-21 15:17:15 +0100

    cleaning up code

--- Elana_project/Assets/Script/Player/Player.cs
+++ Elana_project/Assets/Script/Player/Player.cs
@@ -4,44 +4,123 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Player components")]
     public Rigidbody2D rb;
     public PlayerInput playerInput;
-    public Vector2 moveInput;
+    
+    [Header("Movement")]
+    
     [SerializeField] private float _speed=2f;
+    public float jumpForce;
+    public float jumpCut = 0.5f;
     [SerializeField]private int faceDirection=1;
+    
+    [Header("Input Tracking")]
+    public Vector2 moveInput;
+    private bool jumpPresed;
+    private bool jumpReleased;
+    
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isGrounded;
 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
 
     private void Update()
     {
+        
         Flip();
     }
 
 
     void FixedUpdate()
     {
-        float targetedSpeed = moveInput.x * _speed;
-        rb.linearVelocity = new Vector2(targetedSpeed, rb.linearVelocity.y);
+        CheckGrounded();
+        HandleMovement(); 
+        HandleJump();
         
     }
 
+    #region Handling Region
+
+        private void HandleMovement()
+        {
+            float targetedSpeed = moveInput.x * _speed;
+            rb.linearVelocity = new Vector2(targetedSpeed, rb.linearVelocity.y);
     
-    public void OnMove(InputValue inputValue)
-    {
-        moveInput=inputValue.Get<Vector2>();
-    }
-
-    void Flip()
-    {
-        if (moveInput.x > 0.1f)
+        }
+    
+        private void HandleJump()
         {
-            faceDirection = 1;
+            if (jumpPresed && isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+                jumpPresed = false;
+                jumpReleased = false;
+            }
+    
+            if (jumpReleased)
+            {
+                if (rb.linearVelocity.y > 0)
+                {
+                    rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocity.y*jumpCut);
+                } 
+                jumpReleased = false;
+            }
+        }
+    
+        void CheckGrounded()
+        {
+            isGrounded=Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        }
+    
+        void Flip()
+        {
+            if (moveInput.x > 0.1f)
+            {
+                faceDirection = 1;
+            }
+    
+            else if (moveInput.x < -0.1f)
+            {
+                faceDirection = -1;
+            }
+            transform.localScale = new Vector3(faceDirection, 1f, 1f);
         }
 
-        else if (moveInput.x < -0.1f)
+
+    #endregion
+
+
+
+    #region InputCheck
+
+        public void OnMove(InputValue inputValue)
         {
-            faceDirection = -1;
+            moveInput=inputValue.Get<Vector2>();
         }
-        transform.localScale = new Vector3(faceDirection, 1f, 1f);
-    }
+    
+        public void OnJump(InputValue inputValue)
+        {
+            if (inputValue.isPressed)
+            {
+                jumpPresed = true;
+                jumpReleased = false;
+            }
+            else
+            {
+                jumpReleased = true;
+            }
+        }
+
+    #endregion
+
+    
 }
